3c9c1f015aa79077173aa65d3be947fb9d0da29f
Kersa <128334427+KersaArleta@users.noreply.github.com>
2024-10-31 03:55:08 +0700

    Add files via upload

---
+++ love.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s = "I Love You! ";
+    
+    // Mengatur tinggi hati dari atas ke bawah
+    for(float y = 1.3; y >= -1.1; y -= 0.06) {
+        int index = 0;
+        // Mengatur lebar hati dari kiri ke kanan
+        for(float x = -1.2; x <= 1.2; x += 0.025) {
+            // Persamaan matematika untuk membentuk hati
+            if(pow((x * x + y * y - 1.0), 3) - (x * x * y * y * y) <= 0.0) {
+                cout << s[index % s.size()]; // Mencetak karakter dari string secara berulang
+                index++;
+            } else {
+                cout << ' '; // Mencetak spasi untuk area di luar bentuk hati
+            }
+        }
+        cout << endl; // Pindah ke baris baru setelah setiap baris selesai
+    }
+    
+    return 0;
+}
